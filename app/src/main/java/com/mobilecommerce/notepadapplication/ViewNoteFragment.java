1e1d0ce26029256c6217e0e9cfd8920cd5306f61
Shivjot-Baidwan <shivjotk@gmail.com>
2016-10-03 19:47:39 -0400

    October 3,2016 Shiv

--- app/src/main/java/com/mobilecommerce/notepadapplication/ViewNoteFragment.java
+++ app/src/main/java/com/mobilecommerce/notepadapplication/ViewNoteFragment.java
@@ -1,23 +1,27 @@
 package com.mobilecommerce.notepadapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.R.attr.description;
+import static android.R.attr.icon;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ViewNoteFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
 public class ViewNoteFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
 
     public ViewNoteFragment() {
         // Required empty public constructor
@@ -27,46 +31,19 @@ public class ViewNoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_note, container, false);
+        View layoutFragment = inflater.inflate(R.layout.fragment_view_note, container, false);
+
+        TextView title = (TextView) layoutFragment.findViewById(R.id.viewNoteTitle);
+        TextView body = (TextView) layoutFragment.findViewById(R.id.viewNoteBody);
+        ImageView imageIcon = (ImageView) layoutFragment.findViewById(R.id.noteImage);
+
+        Intent intent = getActivity().getIntent();
+        title.setText(intent.getExtras().getString(MainActivity.Second_Note_Title));
+        body.setText(intent.getExtras().getString(MainActivity.Second_Note_Body));
+        Note.Category noteCategory = (Note.Category) intent.getSerializableExtra(MainActivity.Second_Note_Category);// we have used getSerializable here instead of getString as data type of Category is enum.
+        imageIcon.setImageResource(Note.categoryToDrawble(noteCategory));
+
+        return layoutFragment;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
