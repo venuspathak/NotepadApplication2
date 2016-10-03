0e2d2340a128ccfe977c5c98616ede87621f8f5a
Shivjot-Baidwan <shivjotk@gmail.com>
2016-10-02 23:49:53 -0400

    Commit 2

--- app/src/main/java/com/mobilecommerce/notepadapplication/MainActivity.java
+++ app/src/main/java/com/mobilecommerce/notepadapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobilecommerce.notepadapplication;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,32 +10,22 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //Inflate the emnu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
---
+++ app/src/main/java/com/mobilecommerce/notepadapplication/Note.java
@@ -0,0 +1,64 @@
+package com.mobilecommerce.notepadapplication;
+
+import android.icu.util.ULocale;
+
+
+public class Note {
+    private String title,description;
+    private long noteId, dateCreated;
+    private Category category;
+
+    public enum Category {BILL, FAMILY, FOOD, MAIN, PARTY, PERSONAL, SCHOOL, SHOPPING, THOUGHTS}
+
+    public Note(String title, String description, Category category) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.noteId = noteId;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public  String getDescription() {
+        return description;
+    }
+    public Category getCategory(){
+        return category;
+    }
+    public long getDateCreated() {
+        return dateCreated;
+    }
+    public String toString(){
+        return "ID:" + noteId + "Title" + title + "Description" +description+ "IconID" + category.name() +
+                 "Date:" + dateCreated;
+    }
+    public int getAssociatedDrawble() {
+        return categoryToDrawble(category);
+    }
+
+    public static int categoryToDrawble(Category noteCategory) {
+        switch(noteCategory) {
+            case BILL:
+                return R.drawable.bill;
+            case FAMILY:
+                return R.drawable.family;
+            case FOOD:
+                return R.drawable.food;
+            case MAIN:
+                return R.drawable.main;
+            case PARTY:
+                return R.drawable.party;
+            case PERSONAL:
+                return R.drawable.personal;
+            case SCHOOL:
+                return R.drawable.school;
+            case SHOPPING:
+                return R.drawable.shopping;
+            case THOUGHTS:
+                return R.drawable.thoughts;
+        }
+        return categoryToDrawble(noteCategory);
+    }
+}
